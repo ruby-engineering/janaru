a6246029cc48f5f67e5d2a098cfeb2296ffd00ed
Paweł Dąbrowski <dziamber@gmail.com>
2024-08-04 15:22:31 +0200

    Fix syntax errors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,19 @@
 Rails.application.routes.draw do
-namespace :administration do
-  resources :tags do
+  root "home#index"
 
-end
-endnamespace :administration do
-  resources :categories do
+  namespace :administration do
+    resources :tags
+    resources :categories
+    resources :articles do
+      get :publish, on: :member
+      get :unpublish, on: :member
+    end
+  end
 
-end
-endresources :subscribers, only: [:new, :create] do
+  resources :subscribers, only: [:new, :create] 
+  resources :messages, only: [:new, :create]
+  resources :articles, only: [:show, :index]
 
-endresources :messages, only: [:new, :create] do
-
-endresources :articles, only: [:show, :index] do
-
-endnamespace :administration do
- resources :articles do
-  get :publish, on: :member
-  get :unpublish, on: :member
-
-end
-end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "home#index"
 end
